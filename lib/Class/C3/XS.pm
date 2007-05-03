9ad24e9168a38776ad55ec48b38b21259fdb7e51
Brandon L Black <blblack@gmail.com>
2007-05-03 17:28:20 +0000

    final changes for 0.02 release

--- lib/Class/C3/XS.pm
+++ lib/Class/C3/XS.pm
@@ -4,7 +4,7 @@ use 5.008_000;
 use strict;
 use warnings;
 
-our $VERSION = '0.01_07';
+our $VERSION = '0.02';
 
 =pod
 
@@ -19,14 +19,19 @@ Class::C3::XS - XS speedups for Class::C3
 
 =head1 DESCRIPTION
 
-This contains XS performance enhancers for L<Class::C3>.
-The main L<Class::C3> package will use this package automatically
-if it can find it.  Do not use this package directly, use
-L<Class::C3> instead.
+This contains XS performance enhancers for L<Class::C3> version
+0.16 and higher.  The main L<Class::C3> package will use this
+package automatically if it can find it.  Do not use this
+package directly, use L<Class::C3> instead.
 
 The test suite here is not complete, although it does verify
 a few basic things.  The best testing comes from running the
 L<Class::C3> test suite *after* this module is installed.
+
+This module won't do anything for you if you're running a
+version of L<Class::C3> older than 0.16.  (It's not a
+dependency because it would be circular with the optional
+dep from that package to this one).
 
 =head1 AUTHOR
 
